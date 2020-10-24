fc122d4928f22ec73a1c676f96557c4a18659fc2
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2020-10-24 16:46:24 +0300

    clean utils

--- hivstr/R/utils.R
+++ hivstr/R/utils.R
@@ -22,47 +22,8 @@ check_internet <- function(){
   }
 }
 
-#' Check the status of API request
-#'  @importFrom httr http_error status_code
-#'  @param resp API response.
-check_status <- function(resp){
-  if (httr::http_error(resp)){
-    stop(
-      sprintf(
-        "PSI - MIS API request failed [%s]\n<%s>",
-        status_code(resp),
-        "https://docs.dhis2.org/master/en/developer/html/dhis2_developer_manual.html"
-      ),
-      call. = FALSE
-    )
-  }
-}
-
-#' Set the web api version
-#' @noRd
-api_version <- function(version = NULL){
-  if (is.null(version)){
-    32
-  }
-}
 
 
-#' Set endpoint
-#' @noRd
-api_resource <- function(resource = NULL, push = FALSE){
-
-  if (!is.null(resource) && push == FALSE){
-    paste0(
-      paste("api", api_version(), "analytics", resource, sep = "/")
-    )
-  } else if(!is.null(resource) && push == TRUE){
-    paste0(
-      paste("api", api_version(), resource, sep = "/")
-    )
-  } else{
-    stop("API Resource must be specified!", call. = F)
-  }
-}
 
 colon <- function(...){
   paste0(..., collapse = ":")
