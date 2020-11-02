d1aa3ea1ccbd2f2f129f0f15aebf0cfc4ef6bc5e
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2020-11-02 17:35:31 +0300

    package doc

---
+++ R/hivstr.R
@@ -0,0 +1,10 @@
+#' hivstr: A package to bypass HIV self testing distribution channels
+#'
+#' The hivstr package bypasses HIV Self Testing Distribution Channels, a
+#' category combination filter, directly from the analytic views. It makes it
+#' easier to combine/visualize HIV self testing data with other sources, build
+#' indicators and visualize data on a single chart/table.
+#'
+#' @docType package
+#' @name hivstr
+NULL
